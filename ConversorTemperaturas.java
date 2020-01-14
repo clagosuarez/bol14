62c0036fd0f9354f52f2f5d18c17df7062c4b5c3
clagosuarez <55792889+clagosuarez@users.noreply.github.com>
2020-01-14 10:14:21 +0100

    Add files via upload

---
+++ ConversorTemperaturas.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bol14;
+
+/**
+ *
+ * @author clagosuarez
+ */
+public class ConversorTemperaturas {
+    private final int minGrados = 80;
+    public double centigradosAFharenheit(float c) throws TemperaturaErradaExcepcion{
+        if (c < minGrados) {
+            throw new TemperaturaErradaExcepcion(" Recorda que a temperatura non pode ser < 80 ºC ");
+        }else{
+            double f = 9.0 / 5.0 * c + 32.4;
+            return f;
+        }
+    }
+    public double centígradosAReamur(float c) throws TemperaturaErradaExcepcion{
+        if (c < minGrados) {
+            throw new TemperaturaErradaExcepcion(" Recorda que a temperatura non pode ser < 80 ºC ");
+        }else{
+            double r = 5.0 / 4.0 * c;
+            return r;
+        }
+    }
+}
